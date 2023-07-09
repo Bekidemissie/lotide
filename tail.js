9a6712ee2fc0ba3b2c84c125bd9dbaf723f5d727
Bekidemissie <bekidemissie16@gmail.com>
2023-07-09 15:48:25 -0400

    tail.js frist satge

---
+++ tail.js
@@ -0,0 +1,11 @@
+const tail = function(arr)
+{  let newarray = [];
+
+ for (let i = 1; i < arr.length; i++) {
+     newarray.push(arr[i])
+    
+}  
+  console.log(newarray);
+
+};
+const result = tail(["Hello", "Lighthouse", "Labs"]);
