364ca48c3bf6b743c60db6195d6b26e96863a4dc
Bekidemissie <bekidemissie16@gmail.com>
2023-07-28 14:36:40 -0400

    to add mocha and chai at eqArraysTest functions

--- eqArrays.js
+++ eqArrays.js
@@ -15,7 +15,7 @@ const eqArrays = function(arr1, arr2) {
   };
  
  
-console.log(eqArrays([1, 2, 3], [3, 2, 1]));
-console.log(eqArrays([1, 2, 3], [1, 2, 3]));
+//console.log(eqArrays([1, 2, 3], [3, 2, 1]));
+//console.log(eqArrays([1, 2, 3], [1, 2, 3]));
 //console.log(assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true));
 module.exports = eqArrays;
--- test/eqArraysTest.js
+++ test/eqArraysTest.js
@@ -1,8 +1,19 @@
-const assertEqual = require("../assertEqual");
+const assert = require('chai').assert;
+//const assertEqual = require("../assertEqual");
 const eqArrays = require("../eqArrays");
 
 
+describe("#eqArrays", () => {
+  it(" returns true [1, 2, 3], [1, 2, 3]); ", () => {
+    assert.deepEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+  });
 
- eqArrays([1, 2, 3], [1, 2, 3]); // => true
- eqArrays([1, 2, 3], [3, 2, 1]); // => false
- assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // pass
+it("returns false for [1, 2, 3], [3, 2, 1])", () => {
+    assert.deepEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
+  });
+});
+
+
+ //eqArrays([1, 2, 3], [1, 2, 3]); // => true
+ //eqArrays([1, 2, 3], [3, 2, 1]); // => false
+ //assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // pass
